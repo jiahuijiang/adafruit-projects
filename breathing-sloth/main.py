925e05e5b7f216407c39db319a348f40a73ad5cf
Jiahui Jiang <jiahui@makenotion.com>
2023-02-11 21:38:39 -0500

    breathing slothes

--- breathing-sloth/main.py
+++ breathing-sloth/main.py
@@ -8,7 +8,7 @@ while True:
     motor_1.throttle = 1
     motor_2.throttle = 1
     crickit.drive_1.fraction = 0
-    time.sleep(3)
+    time.sleep(1.7)
     motor_1.throttle = 0
     motor_2.throttle = 0
     crickit.drive_1.fraction = 1
