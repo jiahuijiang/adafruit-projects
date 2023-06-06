543c5cf4682a761d85212efd01964ed436767e45
Jiahui Jiang <jiahui@makenotion.com>
2023-06-05 23:49:27 -0400

    improvement

--- breathing-sloth/main.py
+++ breathing-sloth/main.py
@@ -4,6 +4,10 @@ from adafruit_crickit import crickit
 motor_1 = crickit.dc_motor_1
 motor_2 = crickit.dc_motor_2
 
+## release the residue air
+crickit.drive_1.fraction = 1
+time.sleep(1)
+
 while True:
     motor_1.throttle = 1
     motor_2.throttle = 1
